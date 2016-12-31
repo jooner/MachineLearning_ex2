a4aa88084f94cd85b776b99ca7470541f81efc5e
Octave Online <webmaster@octave-online.net>
2016-12-31 17:20:23 +0000

    Scripted auto-commit

--- gradientDescent.m
+++ gradientDescent.m
@@ -16,12 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-    fprintf('%d\n %d\n %d\n DONE \n',size(theta), size((theta' .* X - y)), size(X));
-    theta = theta - alpha / m * (theta' .* X - y)' * X;
-    fprintf('theta 1 : %f. theta 2 : %f \n', theta(1), theta(2));
-    fprintf('cost is: %f \n', computeCost(X, y, theta));
-
-
+    
+    theta = theta - alpha / m * sum((theta' .* X - y).* X);
+    fprintf(computeCost(X,y,theta));
 
 
 

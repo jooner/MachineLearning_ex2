0c6006855ae00fbeb77a88cf68bf7331421cfb03
Octave Online <webmaster@octave-online.net>
2017-01-06 18:39:40 +0000

    Scripted auto-commit

--- plotData.m
+++ plotData.m
@@ -1,27 +1,29 @@
-function plotData(x, y)
-%PLOTDATA Plots the data points x and y into a new figure 
-%   PLOTDATA(x,y) plots the data points and gives the figure axes labels of
-%   population and profit.
+function plotData(X, y)
+%PLOTDATA Plots the data points X and y into a new figure 
+%   PLOTDATA(x,y) plots the data points with + for the positive examples
+%   and o for the negative examples. X is assumed to be a Mx2 matrix.
 
-figure; % open a new figure window
+% Create New Figure
+figure; hold on;
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Plot the training data into a figure using the 
-%               "figure" and "plot" commands. Set the axes labels using
-%               the "xlabel" and "ylabel" commands. Assume the 
-%               population and revenue data have been passed in
-%               as the x and y arguments of this function.
+% Instructions: Plot the positive and negative examples on a
+%               2D plot, using the option 'k+' for the positive
+%               examples and 'ko' for the negative examples.
 %
-% Hint: You can use the 'rx' option with plot to have the markers
-%       appear as red crosses. Furthermore, you can make the
-%       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
 
 
-plot(x, y, 'rx', 'MarkerSize', 10);
-xlabel('Population of City in 10,000s');
-ylabel('Profit in $10,000s');
 
 
-% ============================================================
+
+
+
+
+
+% =========================================================================
+
+
+
+hold off;
 
 end

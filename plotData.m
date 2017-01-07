88b86b83ca3df2cc9c34fb08ddeaac790b19a3f9
Joon Yang <joony2013@gmail.com>
2017-01-07 15:25:51 +0200

    Update plotData.m

--- plotData.m
+++ plotData.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function plotData(X, y)
 %PLOTDATA Plots the data points X and y into a new figure 
 %   PLOTDATA(x,y) plots the data points with + for the positive examples
@@ -14,8 +13,11 @@ figure; hold on;
 %
 
 
+pos = find(y==1);
+neg = find(y==0);
 
-
+plot(X(pos,1),X(pos,2),'k+','Linewidth',1,'MarkerSize',3);
+plot(X(neg,1),X(neg,2),'ko', 'MarkerFaceColor', 'y','MarkerSize',3);
 
 
 
@@ -26,32 +28,5 @@ figure; hold on;
 
 
 hold off;
-=======
-function plotData(x, y)
-%PLOTDATA Plots the data points x and y into a new figure 
-%   PLOTDATA(x,y) plots the data points and gives the figure axes labels of
-%   population and profit.
-
-figure; % open a new figure window
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Plot the training data into a figure using the 
-%               "figure" and "plot" commands. Set the axes labels using
-%               the "xlabel" and "ylabel" commands. Assume the 
-%               population and revenue data have been passed in
-%               as the x and y arguments of this function.
-%
-% Hint: You can use the 'rx' option with plot to have the markers
-%       appear as red crosses. Furthermore, you can make the
-%       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
-
-
-plot(x, y, 'rx', 'MarkerSize', 10);
-xlabel('Population of City in 10,000s');
-ylabel('Profit in $10,000s');
-
-
-% ============================================================
->>>>>>> origin/master
 
 end
